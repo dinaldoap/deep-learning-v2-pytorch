cdf3ddea6af36f918bf4f0ed696b224902549827
dinaldoap <dinaldoap@gmail.com>
2019-12-09 13:34:50 +0000

    feat: solve part 4

--- intro-to-pytorch/Part 4 - Fashion-MNIST (Exercises).py
+++ intro-to-pytorch/Part 4 - Fashion-MNIST (Exercises).py
@@ -1,6 +1,6 @@
-# To add a new cell, type '# %%'
-# To add a new markdown cell, type '# %% [markdown]'
-# %% Change working directory from the workspace root to the ipynb file location. Turn this addition off with the DataScience.changeDirOnImportExport setting
+# To add a new cell, type '#%%'
+# To add a new markdown cell, type '#%% [markdown]'
+#%% Change working directory from the workspace root to the ipynb file location. Turn this addition off with the DataScience.changeDirOnImportExport setting
 # ms-python.python added
 import os
 try:
@@ -8,10 +8,10 @@ try:
 	print(os.getcwd())
 except:
 	pass
-# %%
+#%%
 from IPython import get_ipython
 
-# %% [markdown]
+#%% [markdown]
 # # Classifying Fashion-MNIST
 # 
 # Now it's your turn to build and train a neural network. You'll be using the [Fashion-MNIST dataset](https://github.com/zalandoresearch/fashion-mnist), a drop-in replacement for the MNIST dataset. MNIST is actually quite trivial with neural networks where you can easily achieve better than 97% accuracy. Fashion-MNIST is a set of 28x28 greyscale images of clothes. It's more complex than MNIST, so it's a better representation of the actual performance of your network, and a better representation of datasets you'll use in the real world.
@@ -22,8 +22,10 @@ from IPython import get_ipython
 # 
 # First off, let's load the dataset through torchvision.
 
-# %%
+#%%
 import torch
+from torch import nn
+from torch import optim
 from torchvision import datasets, transforms
 import helper
 
@@ -38,22 +40,29 @@ trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
 testset = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, train=False, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True)
 
-# %% [markdown]
+#%% [markdown]
 # Here we can see one of the images.
 
-# %%
+#%%
 image, label = next(iter(trainloader))
 helper.imshow(image[0,:]);
 
-# %% [markdown]
+#%% [markdown]
 # ## Building the network
 # 
 # Here you should define your network. As with MNIST, each image is 28x28 which is a total of 784 pixels, and there are 10 classes. You should include at least one hidden layer. We suggest you use ReLU activations for the layers and to return the logits or log-softmax from the forward pass. It's up to you how many layers you add and the size of those layers.
 
-# %%
-# TODO: Define your network architecture here
+#%%
+input_size = image[0].shape[1] * image[0].shape[2]
+hidden_layer_size = 128
+output_layer_size = 10
+model = nn.Sequential(nn.Linear(input_size, hidden_layer_size),
+                      nn.ReLU(),
+                      nn.Linear(hidden_layer_size, output_layer_size),
+                      nn.ReLU(),
+                      nn.LogSoftmax(dim=1))
 
-# %% [markdown]
+#%% [markdown]
 # # Train the network
 # 
 # Now you should create your network and train it. First you'll want to define [the criterion](http://pytorch.org/docs/master/nn.html#loss-functions) ( something like `nn.CrossEntropyLoss`) and [the optimizer](http://pytorch.org/docs/master/optim.html) (typically `optim.SGD` or `optim.Adam`).
@@ -67,19 +76,33 @@ helper.imshow(image[0,:]);
 # 
 # By adjusting the hyperparameters (hidden units, learning rate, etc), you should be able to get the training loss below 0.4.
 
-# %%
-# TODO: Create the network, define the criterion and optimizer
+#%%
+# Create the network, define the criterion and optimizer
+criterion = nn.NLLLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.1)
 
 
-# %%
-# TODO: Train the network here
+#%%
+# Train the network here
+for epoch in range(10):
+  running_loss = 0
+  for images, labels in trainloader:
+    inputs = images.view(images.shape[0], -1)
+    optimizer.zero_grad()
+    outputs = model.forward(inputs)
+    loss = criterion(outputs, labels)
+    loss.backward()
+    optimizer.step()
+    running_loss += loss.item()
+  else:
+    print('Running loss in epoch {}: {}'.format(epoch, running_loss/len(trainloader)))
 
-
-# %%
+#%%
 get_ipython().run_line_magic('matplotlib', 'inline')
 get_ipython().run_line_magic('config', "InlineBackend.figure_format = 'retina'")
 
 import helper
+import math
 
 # Test out your network!
 
@@ -89,10 +112,13 @@ img = images[0]
 # Convert 2D image to 1D vector
 img = img.resize_(1, 784)
 
-# TODO: Calculate the class probabilities (softmax) for img
-ps = 
+# Calculate the class probabilities (softmax) for img
+ps = torch.exp(model.forward(img))
 
 # Plot the image and probabilities
 helper.view_classify(img.resize_(1, 28, 28), ps, version='Fashion')
 
 
+
+
+# %%
